a53141493fa9ca60eaa49592a8a877165c811b29
Eoin Ocathasaigh <eoinocathasaigh1@gmail.com>
2025-11-26 16:41:56 +0000

    Added hardcoded vulnerability

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -11,3 +11,5 @@ from . import views  # noqa: F401
 
 # Time-saver: uncomment to quickly open the example URL in a local terminal
 # print('http://127.0.0.1:5000/hello/VSCode')
+
+AWS_SECRET_KEY = "AKIA1234567890"
